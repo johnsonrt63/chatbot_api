0b913eec7ec70da9acf4c22e8e4d9660ea029c7e
Rich Johnson <48325769+johnsonrt63@users.noreply.github.com>
2026-09-18 20:07:43 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 docker run -p 8000:8000 \
--e OPENAI_API_KEY=your_openai_key \
+-e OPENAI_API_KEY= "$OPEN_API_KEY" \
 -e OPENAI_MODEL=gpt-4o-mini \
 -e SERVICE_API_KEY=$SERVICE_API_KEY \
 -e SYSTEM_PROMPT="You are a helpful assistant." \
